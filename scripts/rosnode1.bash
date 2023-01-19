82b3f5658b910dcbe1ff7ac575f684b1cca4079c
Arjo Chakravarty <arjo@openrobotics.org>
2023-01-19 07:50:07 +0000

    whoops bug in shell script

--- scripts/rosnode1.bash
+++ scripts/rosnode1.bash
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 . /ros/install/setup.bash
-ros2 topic pub -t 30 /test_topic std_msgs/String "data: Hello" && ros2 topic echo --timeout 10 --once /test_topic std_msgs/String > $RES_DIR.local
+ros2 topic pub -t 30 /test_topic std_msgs/String "data: Hello" & ros2 topic echo --timeout 40 --once /test_topic std_msgs/String > $RES_DIR.local
