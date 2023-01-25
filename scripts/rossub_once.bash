5c73928f6acd83b5fc35eefc7022846dbba5fa2e
Arjo Chakravarty <arjo@openrobotics.org>
2023-01-25 09:31:12 +0000

    Towards some sort of test matrix.
    This commit adds a bunch of scripts to inch towards some sort of test
    matrix like the one on ROS discourse.
    
    Signed-off-by: Arjo Chakravarty <arjo@openrobotics.org>

--- scripts/rossub_once.bash
+++ scripts/rossub_once.bash
@@ -1,4 +1,4 @@
 #!/bin/bash
-
+echo "Subnet setting is $RMW_AUTOMATIC_DISCOVERY_RANGE_SUBNET and results are in /results/$RES_DIR/$(hostname)"
 . /ros/install/setup.bash
-ros2 topic echo --timeout 10 --once /test_topic std_msgs/String > $RES_DIR
+ros2 topic echo --timeout 10 --once /test_topic std_msgs/String > /results/$RES_DIR/$(hostname)
--- test_scenario_no_build.bash
+++ test_scenario_no_build.bash
@@ -3,21 +3,16 @@
 CURR_DIR=$(pwd)
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-#if [ "$#" != "1" ]; then
-#    echo "Usage: ./test_scenario.bash <ros2 checkout workspace>"
-#    exit 1
-#fi
-
-# Build the docker image
-#mkdir -p $1/.ros_network_playground
-#cp $SCRIPT_DIR/Dockerfile $1/.ros_network_playground
-#cd $1
-#docker build --build-arg SOURCE_DIR=. -t ros2_test_env -f .ros_network_playground/Dockerfile .
-
-
 cd $SCRIPT_DIR
 mkdir -p results
-docker-compose up
-docker-compose down
+
+for scenario in scenarios/*; do 
+    dirname=`grep RES_DIR $scenario`
+    dirname=$(echo $dirname | sed s/RES_DIR=//g)
+    mkdir -p results/$dirname
+    echo "Loading scenario $scenario"
+    docker-compose --env-file $scenario up
+    docker-compose --env-file $scenario down
+done
 
 cd $CURR_DIR
