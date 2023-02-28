6049332d2b57de1b69e63995022e278d39a1723a
Michael X. Grey <grey@openrobotics.org>
2023-02-28 14:24:15 +0800

    Fix flakiness in test scripts
    Signed-off-by: Michael X. Grey <grey@openrobotics.org>

--- scripts/samehost.bash
+++ scripts/samehost.bash
@@ -1,12 +1,13 @@
 #!/bin/bash
 . /ros/install/setup.bash
 echo "Same host publisher setting is $ROS_AUTOMATIC_DISCOVERY_RANGE, static peers is ${ROS_STATIC_PEERS}"
+# sleep 10
 
 mkdir -p /results/no_discovery_no_static
 ROS_AUTOMATIC_DISCOVERY_RANGE=OFF ROS_STATIC_PEERS="" ros2 topic echo --timeout 15 --once /test_topic std_msgs/String > /results/no_discovery_no_static/subscriber &
 
 mkdir -p /results/localhost_no_static
-ROS_AUTOMATIC_DISCOVERY_RANGE=LOCALHOST ROS_STATIC_PEERS="" ros2 topic echo --timeout 15 --once /test_topic std_msgs/String > /results/localhost_no_static/subscriber &
+ROS_AUTOMATIC_DISCOVERY_RANGE=LOCALHOST ROS_STATIC_PEERS="" ros2 topic echo --timeout 15 --once /test_topic std_msgs/String |& tee /results/localhost_no_static/subscriber &
 
 mkdir -p /results/subnet_no_static
 ROS_AUTOMATIC_DISCOVERY_RANGE=SUBNET ROS_STATIC_PEERS="" ros2 topic echo --timeout 15 --once /test_topic std_msgs/String > /results/subnet_no_static/subscriber &
@@ -19,4 +20,5 @@ ROS_AUTOMATIC_DISCOVERY_RANGE=LOCALHOST ROS_STATIC_PEERS="10.0.0.2" ros2 topic e
 
 mkdir -p /results/subnet_static
 ROS_AUTOMATIC_DISCOVERY_RANGE=SUBNET ROS_STATIC_PEERS="10.0.0.2" ros2 topic echo --timeout 15 --once /test_topic std_msgs/String > /results/subnet_static/subscriber &
-ros2 topic pub -t 10 --max-wait-time 5 /test_topic std_msgs/String "data: Hello"
+
+ros2 topic pub -t 10 -w 0 --max-wait-time 10 /test_topic std_msgs/String "data: Hello"
--- scripts/subscriber.bash
+++ scripts/subscriber.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "Subnet setting is $RMW_AUTOMATIC_DISCOVERY_RANGE_SUBNET and setting name is ${RES_DIR}"
 . /ros/install/setup.bash
-
-ros2 topic echo --timeout 15 --once /test_topic std_msgs/String > /results/subscriber
+echo "Discovery setting is $ROS_AUTOMATIC_DISCOVERY_RANGE, and scenario name is ${RES_DIR}"
+# sleep 5
+ros2 topic echo --timeout 15 --once /test_topic std_msgs/String |& tee /results/subscriber
